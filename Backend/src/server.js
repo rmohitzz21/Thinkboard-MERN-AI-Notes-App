acb859a160e3cf6acf80e19fbda8f6ce81e5d5ab
rmohitzz21 <rmohit21264@gmail.com>
2025-08-13 11:19:56 +0530

    Prepare for deploy

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,14 +1,15 @@
 // import express from 'express';
 // const express = require('express');
-import express from "express";
+import express, { response } from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import dotenv from "dotenv";
 import { connectDb } from "./config/db.js";
 import rateLimiter from "../middleware/rateLimiter.js";
 import cors from 'cors';
-
+import path from 'path';
 dotenv.config();
 const app = express();
+const __dirname = path.resolve()
 
 
 app.use(cors({
@@ -29,6 +30,23 @@ app.use(rateLimiter);
 // })
 
 app.use("/api/notes", notesRoutes);
+
+// Production
+app.use(express.static(path.join(__dirname,"../Frontend/dist")))
+
+app.get("/*",(request,response) => {
+    response.sendFile(path.join(__dirname,"../Frontend","dist","index.html"));
+});
+
+if(process.env.NODE_ENV === "production"){ 
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+}
+
+
 
 connectDb().then( ()=>{
     app.listen(process.env.PORT, () => {
